2ea18bba5dfb8db14319e84eae0b7d62a2bf5158
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-02-16 10:28:28 -0300

    📝 :pencil: [Minor] Home page - basic content

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,3 +1,5 @@
+import React from 'react'
+
 import { StatusBar } from 'expo-status-bar';
 import { Button, SafeAreaView, Image, ButtonContainer, StyleSheet, Text, View } from './styles';
 
@@ -8,6 +10,12 @@ export default function Home() {
         <>
             <StatusBar style="theme-dark" />
             <SafeAreaView>
+                <View>
+                    <Text>Pedir comida não foi tão fácil</Text>
+                    <Image source={banner} />
+                    <Text>Permitir localização</Text>
+                    <Text>Para descobrir restaurantes que entregam em sua região</Text>
+                </View>
                 <ButtonContainer>
                     <Button text="PULAR" onPress={() => { }} />
                     <Button text="ENTRAR" theme="primary" />
