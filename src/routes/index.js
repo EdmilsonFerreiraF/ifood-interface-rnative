b19ced729cb5ac0a44def911e753ed932cb87e6c
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-02-19 23:10:54 -0300

    🚀 :rocket: [Add] Routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,14 +3,20 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import Home from "../pages/Home";
+import Main from "../pages/Main";
 
 const Stack = createStackNavigator();
+
+const screenOptions = {
+    headerShown: false
+}
 
 const Routes = () => {
     return (
         <NavigationContainer>
-            <Stack.Navigator>
+            <Stack.Navigator screenOptions={screenOptions}>
                 <Stack.Screen name="Home" component={Home} />
+                <Stack.Screen name="Main" component={Main} />
             </Stack.Navigator>
         </NavigationContainer>
     )
