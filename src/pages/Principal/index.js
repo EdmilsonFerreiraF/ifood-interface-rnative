9430399eb25f39e3f2dfc31adfd8ccd53eaef4be
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-02-19 22:36:54 -0300

    🚀 :rocket: [Add] Principal page - Restaurant list

--- src/pages/Principal/index.js
+++ src/pages/Principal/index.js
@@ -1,19 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState, useContext, useEffect } from 'react'
-import { SafeAreaView, Image, View, ButtonContainer} from './styles'
-import { Test, Alert, ActivityIndicator } from 'react-native'
+import { Text, View, Alert, ActivityIndicator } from 'react-native'
+import RestauranteItem from '../../components/RestauranteItem';
+import {
+    SafeAreaView,
+    ViewActivity,
+    CategoriesView,
+    BannerView,
+    ViewPrincipal,
+    ViewRestaurantes,
+    ButtonTipoSelect,
+    TextTipoSelect,
+    SelectTipo
+} from './styles'
 
 export default function Principal() {
     const [banner, setBanner] = useState([])
     const [categorias, setCategorias] = useState([])
     const [restaurantes, setRestaurantes] = useState([])
     const [loaded, setLoaded] = useState(false)
-    
+
     useEffect(() => {
         async function buscaDados() {
             try {
                 const response = await fetch('http://my-json-server.typicode.com/pablodev/app-ifood-clone/db')
-                
+
                 const data = await response.json()
 
                 setLoaded(true)
@@ -22,17 +33,48 @@ export default function Principal() {
                 setCategorias(data.categorias)
                 setRestaurantes(data.restaurantes)
             } catch (e) {
-                Alert.alert('Erro ao consultar')
+                Alert.alert('Erro ao consultar' + e)
             }
         }
 
         buscaDados()
     }, [])
-    
+
+    const ViewHome = (props) => {
+        return (
+            <ViewPrincipal>
+                <TituloRestaurantes>Restaurantes</TituloRestaurantes>
+                <ViewRestaurantes>
+                    {restaurantes.map(restaurante => (
+                        <RestauranteItem
+                        key={restaurante.id}
+                        foto={restaurante.url_img}
+                        nota={restaurante.nota}
+                        nome={restaurante.nome}
+                        categoria={restaurante.categoria}
+                        distancia={restaurante.distancia}
+                        valorFrete={restaurante.valor_frete}
+                        tempoEntrega={restaurante.tempo_entrega}
+                         />
+                    ))}
+                </ViewRestaurantes>
+            </ViewPrincipal>
+        )
+    }
+
     return (
         <>
             <StatusBar style="theme-dark" />
             <SafeAreaView>
+                {loaded ? (
+                    <ViewHome></ViewHome>
+                ) :
+                    (
+                        <ViewActivity>
+                            <ActivityIndicator color="#F0001A" size="large" />
+                        </ViewActivity>
+                    )
+                }
                 <Text>Perfil</Text>
             </SafeAreaView>
         </>
