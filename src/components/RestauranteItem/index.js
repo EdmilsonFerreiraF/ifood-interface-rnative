0ae6387913b52f0c333bd11df939b0a2c3090390
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-02-21 16:20:50 -0300

    🔨 :hammer: [Fix] Cannot show plates, restaurants
    Undefined RestauranteView to show restaurants instead of
    RestaurantesView

--- src/components/RestauranteItem/index.js
+++ src/components/RestauranteItem/index.js
@@ -1,24 +1,26 @@
 import React from 'react'
 import { Text } from 'react-native'
-import {AntDesign} from '@expo/vector-icons'
-import { RestauranteView, RestauranteFoto, RestauranteInfo } from './styles'
+import { AntDesign } from '@expo/vector-icons'
+import { RestaurantesView, RestauranteFoto, RestauranteInfo } from './styles'
 
-const RestauranteItem = ({foto, nome, key, nota, categoria, distancia, valorFrete, tempoEntrega}) => {
-    return (<RestauranteView>
-        <RestauranteFoto
-        source={{
-            url: foto.trim(),
-            width: 50,
-            height: 50,
-            resizeMode: 'cover'
-        }}
-        />
-        <RestauranteInfo>
-            <Text>{nome}</Text>
-            <Text><AntDesign name="star" size={12} color="#F9A825"/>{nota} - {categoria} - {distancia}</Text>
-            <Text>{tempoEntrega} * R$ {valorFrete}</Text>
-        </RestauranteInfo>
-    </RestauranteView>)
+const RestauranteItem = ({ foto, nome, key, nota, categoria, distancia, valorFrete, tempoEntrega }) => {
+    return (
+        <RestaurantesView>
+            <RestauranteFoto
+                source={{
+                    url: foto.trim(),
+                    width: 50,
+                    height: 50,
+                    resizeMode: 'cover'
+                }}
+            />
+            <RestauranteInfo>
+                <Text>{nome}</Text>
+                <Text><AntDesign name="star" size={12} color="#F9A825" />{nota} - {categoria} - {distancia}</Text>
+                <Text>{tempoEntrega} * R$ {valorFrete}</Text>
+            </RestauranteInfo>
+        </RestaurantesView>
+    )
 }
 
 export default RestauranteItem
